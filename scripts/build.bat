bb5cdcc7b5494bb214b54c0fb5de5262d89480fd
Alexander David Frick <Alex313031@gmail.com>
2023-05-27 11:26:55 -0500

    Update build.bat

--- scripts/build.bat
+++ scripts/build.bat
@@ -5,21 +5,9 @@ set LDFLAGS="-Wl,-O3 -mavx -maes -s"
 set VERBOSE=1
 set V=1
 
-set MSVS_VERSION="2017"
-set GYP_MSVS_VERSION="2017"
+set MSVS_VERSION="2019"
+set GYP_MSVS_VERSION="2019"
 set ELECTRON_CACHE=%~dp0%electron\bin
 set electron_config_cache=%~dp0%electron\bin
 
 npm run burnthemall
-
-rem set NODE_ENV=production
-
-rem npm run dist
-
-rem cd ..
-
-rem python3 -m castlabs_evs.vmp sign-pkg dist\win-unpacked
-
-rem cd ..\scripts
-
-rem npm run dist-win
